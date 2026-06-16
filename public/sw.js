a3baa366f6978706564cfc5ddfacc7d402fca8cf
Vitor Ricardo <vitorfworlds@gmail.com>
2026-06-15 21:14:47 -0300

    fix: bump SW cache to v2 for updated PWA icons

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flowinpay-v1';
+const CACHE_NAME = 'flowinpay-v2';
 const STATIC_ASSETS = ['/dashboard', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
